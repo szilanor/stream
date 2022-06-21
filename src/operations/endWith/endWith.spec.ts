5f30500e9ee71d1f5da2c913c56fa63f0c23d15c
Norbert Szilagyi <norbi.szilagyi95@gmail.com>
2022-06-21 13:46:17 +0200

    Rename operation from lastWith to endWith

--- src/operations/endWith/endWith.spec.ts
+++ src/operations/endWith/endWith.spec.ts
@@ -1,16 +1,16 @@
 import {Stream} from '../../stream';
 import {toArray} from '../../collectors';
-import {lastWith} from './lastWith';
+import {endWith} from './endWith';
 import {empty} from '../../creators';
 
-describe('Operation function: lastWith()', () => {
+describe('Operation function: endWith()', () => {
   test('should return only the value parameter on empty stream', () => {
-    const res = empty<number>().pipe(lastWith(1)).collect(toArray());
+    const res = empty<number>().pipe(endWith(1)).collect(toArray());
     expect(res).toStrictEqual([1]);
   });
 
   test('should prepend the value', () => {
-    const res = new Stream([1, 2, 3]).pipe(lastWith(4)).collect(toArray());
+    const res = new Stream([1, 2, 3]).pipe(endWith(4)).collect(toArray());
     expect(res).toStrictEqual([1, 2, 3, 4]);
   });
 });
