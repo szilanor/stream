543f04c90297556084ec6ab28ead896df0e87291
Norbert Szilagyi <norbi.szilagyi95@gmail.com>
2022-07-28 15:54:29 +0200

    Remove unnecessary code

--- src/operations/flatMap/flatMap.spec.ts
+++ src/operations/flatMap/flatMap.spec.ts
@@ -11,6 +11,14 @@ describe('Operation function: flatMap()', () => {
     expect(res).toStrictEqual([1, 2, 3]);
   });
 
+  test('Example in https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flatMap#for_adding_and_removing_items_during_a_map', () => {
+    const res = new Stream([5, 4, -3, 20, 17, -33, -4, 18])
+      .pipe(flatMap(n => (n < 0 ? [] : n % 2 === 0 ? [n] : [n - 1, 1])))
+      .collect(toArray());
+
+    expect(res).toStrictEqual([4, 1, 4, 20, 16, 1, 18]);
+  });
+
   test('should flatMap iterable entries with the callback function', () => {
     const iterableFor = (n: number) =>
       function* () {
--- src/operations/flatMap/flatMap.ts
+++ src/operations/flatMap/flatMap.ts
@@ -1,34 +1,11 @@
 import type {OperationFunction} from '../../types';
 
-export class FlatMapIterator<T, O> implements IterableIterator<O> {
-  private index = 0;
-
-  constructor(
-    private iterator: Iterator<T>,
-    private mapper: (value: T, index: number) => O
-  ) {}
-
-  [Symbol.iterator](): IterableIterator<O> {
-    return this;
-  }
-
-  next(): IteratorResult<O> {
-    const {value, done} = this.iterator.next();
-    return done
-      ? {done, value: undefined as unknown}
-      : {done, value: this.mapper(value, this.index++)};
-  }
-}
-
 /**
  * Returns an Iterable that yields the inner entries of the
  * result produced by the function.
  */
 export function flatMap<T, O>(
-  func: (
-    value: T,
-    index: number
-  ) => Iterable<O> | Generator<O, unknown, unknown>
+  func: (value: T, index: number) => Iterable<O>
 ): OperationFunction<T, O> {
   return entries =>
     (function* () {
